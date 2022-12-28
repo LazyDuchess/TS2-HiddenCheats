7c13659d00ebee8f5c00917da8a3b723cd87c357
LazyDuchess <42678262+LazyDuchess@users.noreply.github.com>
2022-12-28 05:29:25 -0300

    Comments and edits for clarity.

--- TS2-HiddenCheats/HiddenCheats.cpp
+++ TS2-HiddenCheats/HiddenCheats.cpp
@@ -3,18 +3,26 @@
 #include <Psapi.h>
 #include "LDLibraries/LDHooking/hooking.h"
 
+// Hook "CALL cTSEffectCheat::cTSEffectCheat" ASM line in AddEffectCheats.
 #define ADDEFFECTCHEATS_HOOK1_ADDR 0xffaceb
+
+// Hook "CALL cTSEffectsManagerCheat::cTSEffectsManagerCheat" ASM line in AddEffectCheats.
 #define ADDEFFECTCHEATS_HOOK2_ADDR 0xffad18
+
+// Addresses to jump back to when done hooking.
 
 #define ADDEFFECTCHEATS_HOOK1_RETURN_ADDR (ADDEFFECTCHEATS_HOOK1_ADDR + 5)
 #define ADDEFFECTCHEATS_HOOK2_RETURN_ADDR (ADDEFFECTCHEATS_HOOK2_ADDR + 5)
 
+// Typedefs for functions to call.
+
 typedef void(__thiscall* thiseffectfunction_t)(void *effectsManager);
+typedef void(__stdcall* cheatfunction_t)(void* cheat);
+
 thiseffectfunction_t cTSEffectCheat = (thiseffectfunction_t)0xFFAA10;
 thiseffectfunction_t cTSEffectsManagerCheat = (thiseffectfunction_t)0xFFA870;
 
-typedef void(__stdcall* function_t)(void* cheat);
-function_t TSRegisterTestingCheat = (function_t)0xD0DAA0;
+cheatfunction_t TSRegisterTestingCheat = (cheatfunction_t)0xD0DAA0;
 
 namespace HiddenCheats {
 
@@ -24,15 +32,18 @@ namespace HiddenCheats {
 	__declspec(naked) void AddEffectCheats_Hook1()
 	{
 		__asm {
+			// effectsManager is already pushed onto the stack at this point.
+
 			// command = cTSEffectCheat::cTSEffectCheat(this [ecx],effectsManager);
 			call cTSEffectCheat
 
-			// Custom code below
+			// Custom code below.
+			// TSRegisterTestingCheat(command);
 			push eax
-			// TSRegisterTestingCheat(command)
 			call TSRegisterTestingCheat
 			pop eax
 
+			// Return to original code.
 			jmp returnAddEffectCheatsHook1
 		}
 	}
@@ -40,24 +51,27 @@ namespace HiddenCheats {
 	__declspec(naked) void AddEffectCheats_Hook2()
 	{
 		__asm {
+			// effectsManager is already pushed onto the stack at this point.
+
 			// command = cTSEffectsManagerCheat::cTSEffectsManagerCheat(this [ecx],effectsManager);
 			call cTSEffectsManagerCheat
 
-			// Custom code below
+			// Custom code below.
+			// TSRegisterTestingCheat(command);
 			push eax
-			// TSRegisterTestingCheat(command)
 			call TSRegisterTestingCheat
 			pop eax
 
+			// Return to original code.
 			jmp returnAddEffectCheatsHook2
 		}
 	}
 
+	/// <summary>
+	/// Run hooks.
+	/// </summary>
 	void Run() {
-		returnAddEffectCheatsHook1 = (char*)((BYTE*)ADDEFFECTCHEATS_HOOK1_ADDR + 5);
 		Hooking::MakeJMP((BYTE*)ADDEFFECTCHEATS_HOOK1_ADDR, (DWORD)AddEffectCheats_Hook1, 5);
-
-		returnAddEffectCheatsHook2 = (char*)((BYTE*)ADDEFFECTCHEATS_HOOK2_ADDR + 5);
 		Hooking::MakeJMP((BYTE*)ADDEFFECTCHEATS_HOOK2_ADDR, (DWORD)AddEffectCheats_Hook2, 5);
 	}
 }
